1122e773fa0848e999b4411f3322ac2db8f9e9f0
Cristóbal Carnero Liñán <ccarnerolinan@gmail.com>
2018-02-27 16:47:07 +0100

    Fixes.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -12,7 +12,12 @@ function fish_prompt
     powerline-shell --shell bare $status
 end
 
-set PATH $PATH $HOME/bin $HOME/.local/bin /snap/bin $HOME/Programas/anaconda3/bin/
+set PATH $PATH $HOME/bin $HOME/.local/bin /snap/bin
+
+# Anaconda
+set PATH $PATH $HOME/Programas/anaconda3/bin/
+source $HOME/Programas/anaconda3/etc/fish/conf.d/conda.fish
+# set fish_user_paths $fish_user_paths $HOME/Programas/anaconda3/bin/
 
 # Disable copy deleted words to clipboard
 set FISH_CLIPBOARD_CMD "cat"
@@ -25,7 +30,5 @@ if test -e ~/.nvm/nvm.sh
 	bass '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'  # This loads nvm
 end
 
-# Fix error "QXcbConnection: Could not connect to display :0"
-# set -x QT_QPA_PLATFORM offscreen
-
 set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
+powerline-setup
