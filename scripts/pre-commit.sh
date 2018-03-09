d54b617ad7716f7c5dd66ca7699f3b0e29fd88d8
Cristóbal Carnero Liñán <ccarnerolinan@gmail.com>
2018-03-09 15:02:38 +0100

    Git pre-hook for test before commit.

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,24 @@
+# pre-commit.sh
+#
+# Based on http://codeinthehole.com/tips/tips-for-using-a-git-pre-commit-hook/
+#
+# Add to your git project like this:
+# $ ln -s ~/scripts/pre-commit.sh .git/hooks/pre-commit
+
+if [ -e scripts/test.sh ]; then
+    STASH_NAME="pre-commit-$(date +%s)"
+    git stash save -q --keep-index $STASH_NAME
+
+    scripts/test.sh
+    RESULT=$?
+
+    STASHES=$(git stash list)
+    if [[ $STASHES == "$STASH_NAME" ]]; then
+        git stash pop -q
+    fi
+
+    [ $RESULT -ne 0 ] && exit 1
+    exit 0
+else
+    echo "Warning: no test script!"
+fi
