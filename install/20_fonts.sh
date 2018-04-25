899dd3e5a848d63d2548307d5778fab40cc4cc74
Cristóbal Carnero Liñán <cristobal@virtuallylive.com>
2018-04-25 10:13:38 +0200

    Using powerline-shell instead powerline. Minor fixes.

--- install/20_fonts.sh
+++ install/20_fonts.sh
@@ -4,6 +4,7 @@ set -e
 
 FONTS_PATH="${HOME}/.fonts"
 FONT_NAME="Hack_Regular_Nerd_Font_Complete_Mono.ttf"
+FONT_URL='https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf'
 
 cd "$(dirname "$0")/.."
 
@@ -12,8 +13,10 @@ source lib/colors.sh
 if [[ ! -n $(find ${FONTS_PATH} -iname ${FONT_NAME}) ]]; then
     echo "==> ${LBLUE}Installing Hack patched font…${END}"
     mkdir -p "${FONTS_PATH}"
-    curl -fLo "${FONTS_PATH}/${FONT_NAME}" 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf'
+    curl -fLo "${FONTS_PATH}/${FONT_NAME}" ${FONT_URL}
 fi
+
+fc-cache -vf ${FONTS_PATH}
 
 echo "==> ${LBLUE}Linking Konsole and Yakuake configuration…${END}"
 mkdir -p ~/.kde/share/apps/konsole
