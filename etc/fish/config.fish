2870dc2feb5e85faaf887e2d8d60154f4595b788
Cristóbal Carnero Liñán <ccarnerolinan@gmail.com>
2018-04-25 10:13:38 +0200

    Using powerline-shell instead powerline. Minor fixes.

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,5 +1,11 @@
 . ~/.config/fish/aliases.fish
 
+# Disable copy deleted words to clipboard
+set FISH_CLIPBOARD_CMD "cat"
+
+# set -gx EDITOR vim
+# set -U EDITOR vim
+
 function fish_greeting
 	set_color $fish_color_autosuggestion
 	uname -nmsr
@@ -8,11 +14,30 @@ function fish_greeting
 	set_color normal
 end
 
-function fish_prompt
-    powerline-shell --shell bare $status
+if type -q powerline-shell
+    function fish_prompt
+        powerline-shell --shell bare $status
+    end
+else
+    set PYTHON3_USER_PATH (python3 -c "import sys; print(next((x for x in sys.path if x.startswith(\"$HOME\")), ['']))")
+
+    if test -e "$PYTHON3_USER_PATH/powerline/bindings/fish/"
+        set fish_function_path $fish_function_path "$PYTHON3_USER_PATH/powerline/bindings/fish/"
+        powerline-setup
+    end
 end
 
-set PATH $PATH $HOME/bin $HOME/.local/bin /snap/bin
+if test -e $HOME/bin
+    set PATH $PATH $HOME/bin
+end
+
+if test -e $HOME/.local/bin
+    set PATH $PATH $HOME/.local/bin /snap/bin
+end
+
+if test -e /snap/bin
+    set PATH $PATH /snap/bin
+end
 
 set LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda/extras/CUPTI/lib64/ /usr/local/cuda-9.0/lib64
 
@@ -22,20 +47,13 @@ if test -e /usr/lib/jvm/java-8-openjdk-amd64/
 end
 
 # Anaconda
-set PATH $PATH $HOME/Programas/anaconda3/bin/
-source $HOME/Programas/anaconda3/etc/fish/conf.d/conda.fish
-# set fish_user_paths $fish_user_paths $HOME/Programas/anaconda3/bin/
-
-# Disable copy deleted words to clipboard
-set FISH_CLIPBOARD_CMD "cat"
-
-# set -gx EDITOR vim
-# set -U EDITOR vim
+if test -e $HOME/Programas/anaconda3/bin/
+    set PATH $PATH $HOME/Programas/anaconda3/bin/
+    source $HOME/Programas/anaconda3/etc/fish/conf.d/conda.fish
+    # set fish_user_paths $fish_user_paths $HOME/Programas/anaconda3/bin/
+end
 
 if test -e ~/.nvm/nvm.sh
 	bass export NVM_DIR="$HOME/.nvm"
 	bass '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"'  # This loads nvm
 end
-
-set fish_function_path $fish_function_path "$HOME/.local/lib/python2.7/site-packages/powerline/bindings/fish"
-powerline-setup
