8f6c76138524ba9a0b00c638e0ce8356d7ae7c20
Cristóbal Carnero Liñán <ccarnerolinan@gmail.com>
2018-07-01 20:23:35 +0200

    Different behaviour between server & desktop (WIP).

--- install/20_powerline.sh
+++ install/20_powerline.sh
@@ -2,29 +2,11 @@
 
 set -e
 
-FONTS_PATH="${HOME}/.fonts"
-# FONTS_PATH="${HOME}.local/share/fonts"
-FONT_NAME="Hack_Regular_Nerd_Font_Complete_Mono.ttf"
-FONT_URL='https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf'
-
 cd "$(dirname "$0")/.."
 
 source lib/files.sh
 source lib/colors.sh
 
-if [[ ! -n $(find ${FONTS_PATH} -iname ${FONT_NAME}) ]]; then
-    echo "==> ${LBLUE}Installing Hack nerd font…${END}"
-    directory "${FONTS_PATH}"
-    download ${FONT_URL} "${FONTS_PATH}/${FONT_NAME}"
-fi
-
-fc-cache -vf ${FONTS_PATH}
-
 echo "==> ${LBLUE}Linking powerline-shell config…${END}"
 directory ~/.config/powerline-shell
 linked $(pwd)/etc/powerline-shell.json ~/.config/powerline-shell/config.json
-
-echo "==> ${LBLUE}Linking Konsole and Yakuake configuration…${END}"
-directory ~/.kde/share/apps/konsole
-linked $(pwd)/etc/kde/share/apps/konsole/Shell.profile ~/.kde/share/apps/konsole/Shell.profile
-linked $(pwd)/etc/kde/share/apps/konsole/Yakuake.profile ~/.kde/share/apps/konsole/Yakuake.profile
--- install/40_xbindkeys.sh
+++ install/40_xbindkeys.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-cd "$(dirname "$0")/.."
+if ! xset q &>/dev/null; then
+    cd "$(dirname "$0")/.."
 
-source lib/files.sh
-source lib/colors.sh
+    source lib/files.sh
+    source lib/colors.sh
 
-echo "==> ${LBLUE}Linking xbindkeys configuration…${END}"
-linked $(pwd)/etc/xbindkeysrc ~/.xbindkeysrc
+    echo "==> ${LBLUE}Linking xbindkeys configuration…${END}"
+    linked $(pwd)/etc/xbindkeysrc ~/.xbindkeysrc
 
-if ! pgrep xbindkeysx &> /dev/null; then
-    echo "==> ${LBLUE}Launching xbindkeys…${END}"
-    xbindkeys -f ~/.xbindkeysrc
+    if ! pgrep xbindkeysx &> /dev/null; then
+        echo "==> ${LBLUE}Launching xbindkeys…${END}"
+        xbindkeys -f ~/.xbindkeysrc
+    fi
 fi
